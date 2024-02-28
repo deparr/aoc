8b28fd45d4782e6eda6fa6a978d3d45501225fe9
deparr <dparrott@protonmail.com>
2024-02-27 20:15:57 -0700

    wip: Tue Feb 27 08:15:57 PM MST 2024

--- 2023/days/day-01/main.go
+++ 2023/days/day-01/main.go
@@ -6,5 +6,30 @@ import (
 )
 
 func main() {
+    lines := utils.ReadLines()
+    var res int64 = 0
+    var left byte
+    var right byte
+    for _, line := range lines {
 
+        for i := 0; i < len(line); i++ {
+            if line[i] >= '0' && line[i] <= '9' {
+                left = line[i] - '0'
+                break;
+            }
+
+        }
+
+        for i := len(line)-1; i >= 0; i-- {
+            if line[i] >= '0' && line[i] <= '9' {
+                right = line[i] - '0'
+                break;
+            }
+
+        }
+
+        res += int64(left * 10 + right)
+    }
+
+    fmt.Println(res)
 }
