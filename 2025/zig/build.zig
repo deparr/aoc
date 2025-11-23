5ddd8ae2f9d2d816abe86962ac7d9eef031e9c4d
deparr <dparrott@protonmail.com>
2025-11-23 13:49:36 -0700

    zig: fetch input

--- 2025/zig/build.zig
+++ 2025/zig/build.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 var optimize: std.builtin.OptimizeMode = undefined;
 var target: std.Build.ResolvedTarget = undefined;
+var year: []const u8 = undefined;
 
 pub fn build(b: *std.Build) void {
     target = b.standardTargetOptions(.{});
@@ -10,8 +11,9 @@ pub fn build(b: *std.Build) void {
     const new_day_step = b.step("new-day", "stub out the next day");
     const all_step = b.step("all", "build all days");
 
+    year = b.option([]const u8, "year", "year number") orelse "2025";
     const dayno: ?usize = b.option(usize, "day", "day number");
-    const selected_day = dayno orelse get_latest_day(b);
+    const selected_day = dayno orelse getLatestDay(b);
     doNewDayStep(b, new_day_step, selected_day);
 
     if (selected_day == 0) {
@@ -27,19 +29,32 @@ pub fn build(b: *std.Build) void {
     doAllStep(b, all_step, selected_day);
 
     createExecutableForDay(b, b.getInstallStep(), selected_day);
-
 }
 
 fn doNewDayStep(b: *std.Build, create: *std.Build.Step, day: usize) void {
+    const fetch = b.addExecutable(.{
+        .name = "fetch",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/build/fetch-input.zig"),
+            .optimize = .Debug,
+            .target = b.graph.host,
+        }),
+    });
+
+    const run_fetch = b.addRunArtifact(fetch);
+    run_fetch.addArg(year);
+    run_fetch.addArg(b.fmt("{d}", .{day}));
+    run_fetch.setName("fetch day input");
+
     const day_path = b.fmt("src/day{d:02}.zig", .{day + 1});
-    const log = b.addSystemCommand(&.{ "echo", b.fmt("creating {s}...", .{day_path}), " // TODO fetch the input as well" });
-    const cmd = b.addSystemCommand(&.{ "cp", "src/template.zig", day_path });
-    cmd.step.dependOn(&log.step);
-    create.dependOn(&cmd.step);
+    const write_templ = TemplateDayStep.create(b, "src/build/template.zig", day_path);
+
+    create.dependOn(&write_templ.step);
+    create.dependOn(&run_fetch.step);
 }
 
 fn doAllStep(b: *std.Build, all: *std.Build.Step, latest_day: usize) void {
-    for (1..latest_day+1) |day| {
+    for (1..latest_day + 1) |day| {
         createExecutableForDay(b, all, day);
     }
 }
@@ -57,9 +72,7 @@ fn createExecutableForDay(b: *std.Build, step: *std.Build.Step, num: usize) void
         }),
     });
 
-    const log = b.addSystemCommand(&.{ "echo", b.fmt("building {s}", .{source_file})});
     const check_file = b.addCheckFile(source_path, .{});
-    check_file.step.dependOn(&log.step);
     const install = b.addInstallArtifact(exe, .{});
     install.step.dependOn(&check_file.step);
     step.dependOn(&install.step);
@@ -70,7 +83,7 @@ fn fail(b: *std.Build, msg: []const u8) void {
     b.default_step.dependOn(&fail_step.step);
 }
 
-fn get_latest_day(b: *std.Build) usize {
+fn getLatestDay(b: *std.Build) usize {
     var src = std.fs.cwd().openDir("src", .{ .iterate = true }) catch |err| {
         fail(b, b.fmt("{t}\n", .{err}));
         return 0;
@@ -100,3 +113,37 @@ fn get_latest_day(b: *std.Build) usize {
 
     return latest_day;
 }
+
+const TemplateDayStep = struct {
+    step: std.Build.Step,
+    builder: *std.Build,
+    source: []const u8,
+    dest: []const u8,
+
+    pub fn create(b: *std.Build, source: []const u8, dest: []const u8) *TemplateDayStep {
+        const self = b.allocator.create(TemplateDayStep) catch @panic("OOM");
+        self.* = .{
+            .step = std.Build.Step.init(.{
+                .id = .custom,
+                .name = b.fmt("copy {s} to {s}", .{ source, dest }),
+                .owner = b,
+                .makeFn = make,
+            }),
+            .builder = b,
+            .source = b.dupe(source),
+            .dest = b.dupe(dest),
+        };
+        return self;
+    }
+
+    fn make(step: *std.Build.Step, _: std.Build.Step.MakeOptions) anyerror!void {
+        const self: *TemplateDayStep = @fieldParentPtr("step", step);
+
+        try std.fs.cwd().copyFile(
+            self.source,
+            std.fs.cwd(),
+            self.dest,
+            .{},
+        );
+    }
+};
---
+++ 2025/zig/src/build/fetch-input.zig
@@ -0,0 +1,62 @@
+const std = @import("std");
+
+const url = "https://adventofcode.com/{s}/day/{s}/input";
+
+pub fn main() !void {
+    var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
+    const gpa = debug_allocator.allocator();
+    defer _ = debug_allocator.deinit();
+
+    const root = std.Progress.start(.{
+        .root_name = "Fetch input",
+        .estimated_total_items = 1,
+    });
+    defer root.end();
+
+    const args = try std.process.argsAlloc(gpa);
+    defer std.process.argsFree(gpa, args);
+
+    if (args.len < 3)
+        return error.MissingExpectedArgs;
+    const year = args[1];
+    const day = args[2];
+
+    const cookie_raw = std.process.getEnvVarOwned(gpa, "AOC_COOKIE") catch |err| switch (err) {
+        error.EnvironmentVariableNotFound => try std.fs.cwd().readFileAlloc(gpa, "aoc-cookie", 4096),
+        else => return err,
+    };
+    defer gpa.free(cookie_raw);
+    const cookie = std.mem.trim(u8, cookie_raw, &std.ascii.whitespace);
+
+    std.fs.cwd().makeDir("input") catch |err| switch (err) {
+        error.PathAlreadyExists => {},
+        else => return err,
+    };
+
+    var pathbuf: [16]u8 = undefined;
+    var output_file = try std.fs.cwd().createFile(try std.fmt.bufPrint(&pathbuf, "input/{s}", .{day}), .{});
+    var file_buf: [256]u8 = undefined;
+    var file_writer = output_file.writer(&file_buf);
+
+    var client: std.http.Client = .{ .allocator = gpa };
+    var cookie_buf: [1024]u8 = undefined;
+    var url_buf: [1024]u8 = undefined;
+    var headers = [_]std.http.Header{.{
+        .name = "Cookie",
+        .value = try std.fmt.bufPrint(&cookie_buf, "session={s}", .{cookie}),
+    }};
+    const res = try client.fetch(.{
+        .method = .GET,
+        .location = .{ .url = try std.fmt.bufPrint(&url_buf, url, .{ year, day }) },
+        .extra_headers = &headers,
+        .response_writer = &file_writer.interface,
+    });
+    client.deinit();
+
+    output_file.close();
+
+    if (res.status != .ok)
+        std.debug.print("fetch failed with status {t}", .{res.status});
+
+    root.completeOne();
+}
