a13612e702a258028608a9362979808848af8f2b
deparr <dparrott@protonmail.com>
2025-12-04 21:01:11 -0700

    2025(vis): day 4
    pretty bad visuals, that's harder than I thought
    not very reusable for adding days either

---
+++ 2025/vis/day_4.gd
@@ -0,0 +1,117 @@
+extends Node2D
+
+const dot = ord(".")
+const ex = ord("x")
+
+var input: String
+var grid: Array[PackedByteArray] = []
+
+@onready var visual: Node2D = %Grid
+
+
+func _ready() -> void:
+	var f := FileAccess.get_file_as_string("res://input/4")
+	if f == "":
+		push_error("unable to open file:", error_string(FileAccess.get_open_error()))
+		return
+	f = f.strip_edges()
+	input = f
+
+	%Solve.pressed.connect(_on_solve_pressed)
+
+
+func _on_solve_pressed() -> void:
+	var res := await solve()
+	print(res)
+
+
+func count_accessible() -> int:
+	var accessible := 0
+	for i in grid.size():
+		var row := grid[i]
+		for j in row.size():
+			var b := row[j]
+			if b == dot:
+				continue
+
+			var blocked := 0
+
+			if j > 0 and row[j - 1] != dot:
+				blocked += 1;
+			
+			if j < row.size() - 1 and row[j + 1] != dot:
+				blocked += 1;
+			
+			if i > 0 and grid[i - 1][j] != dot:
+				blocked += 1;
+			
+			if i < grid.size() - 1 and grid[i + 1][j] != dot:
+				blocked += 1;
+			
+			if j > 0 and i > 0 and grid[i - 1][j - 1] != dot:
+				blocked += 1;
+			
+			if j > 0 and i < grid.size() - 1 and grid[i + 1][j - 1] != dot:
+				blocked += 1;
+			
+			if j < row.size() - 1 and i > 0 and grid[i - 1][j + 1] != dot:
+				blocked += 1;
+			
+			if j < row.size() - 1 and i < grid.size() - 1 and grid[i + 1][j + 1] != dot:
+				blocked += 1;
+						
+
+			if blocked <= 3:
+				accessible += 1
+				grid[i][j] = ex
+
+
+	return accessible
+
+
+func clear_marked() -> void:
+	for row in grid:
+		for i in row.size():
+			if row[i] == ex:
+				row[i] = dot
+
+
+func dump_grid() -> void:
+	for row in grid:
+		print(row.get_string_from_ascii())
+
+
+func update_visual() -> void:
+	visual.queue_redraw()
+
+
+func solve() -> int:
+	var lines := input.split("\n", false)
+	grid.resize(lines.size())
+	for i in grid.size():
+		grid[i] = lines[i].strip_edges().to_ascii_buffer()
+
+	visual.grid = grid
+	visual.grid_size = get_viewport_rect().size / Vector2(float(grid[0].size()), float(grid.size()))
+
+	var accessible := 0
+	var wait_time := 0.25
+	while true:
+		var newly_accessible = count_accessible()
+		accessible += newly_accessible
+		update_visual()
+		await get_tree().create_timer(wait_time).timeout
+		clear_marked()
+		await get_tree().create_timer(wait_time).timeout
+		if newly_accessible == 0:
+			visual.available = visual.done
+		update_visual()
+
+		%Removed.text = "removed paper rolls: %d" % accessible
+		wait_time = maxf(wait_time * 0.9, 0.08)
+
+		if newly_accessible == 0:
+			break
+	
+	%Done.text = "done!"
+	return accessible
---
+++ 2025/vis/day_4_visual.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+const mark_byte = ord('x')
+const open_byte = ord('.')
+const avil_byte = ord('@')
+
+@export var marked := Color(0.9, 0.3, 0.2)
+@export var open := Color(0.1, 0.1, 0.1)
+@export var available := Color(0.4, 0.8, 0.4)
+@export var done := Color(0.2, 0.2, 0.7)
+
+
+@export var grid_size := Vector2i(32, 32)
+
+var grid: Array[PackedByteArray]
+
+func _draw() -> void:
+	if not grid or grid.size() == 0:
+		return
+	var m := grid.size()
+	var n := grid[0].size()
+
+	for i in m:
+		for j in n:
+			var color: Color
+			match grid[i][j]:
+				mark_byte:
+					color = marked
+				open_byte:
+					color = open
+				avil_byte:
+					color = available
+
+			var rect := Rect2(grid_size * Vector2i(j, i), grid_size)
+			draw_rect(rect, color)
